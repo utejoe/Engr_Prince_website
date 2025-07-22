9806586926675e9f77ff7abdf6102d258c3cb6ec
Uyi Obazee <utejoe.ju@gmail.com>
2025-07-22 18:13:33 +0100

    committing a new job

--- js/main.js
+++ js/main.js
@@ -15,3 +15,36 @@ function loadComponent(filePath, selector) {
     })
     .catch(error => console.error("Error loading component:", error));
 }
+
+
+
+
+
+
+function sendEmail(event) {
+  event.preventDefault();
+
+  const fullName = document.getElementById("fullName").value;
+  const email = document.getElementById("email").value;
+  const phoneNumber = document.getElementById("phoneNumber").value;
+  const subject = document.getElementById("subject").value;
+  const message = document.getElementById("message").value;
+
+  const fullMessage = `Sender Name: ${fullName}\nSender Email: ${email}\nPhone Number: ${phoneNumber}\n\n${message}`;
+
+  emailjs.send("service_26cy9jq", "template_th0yens", {
+    from_name: fullName,
+    from_email: email,
+    from_phone: phoneNumber,
+    message_subject: subject,
+    message: fullMessage,
+  })
+  .then(function(response) {
+    document.getElementById("contactForm").reset();
+    alert("✅ Your message has been sent successfully!");
+  })
+  .catch(function(error) {
+    console.error("EmailJS Error:", error);
+    alert("❌ Failed to send message. Please try again.");
+  });
+}
